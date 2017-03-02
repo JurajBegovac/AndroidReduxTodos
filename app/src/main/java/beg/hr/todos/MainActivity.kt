c10f82bc6143c12ed596b6a11db0e5f88afe4f08
Juraj <juraj.begovac@bellabeat.com>
2017-03-02 14:09:27 +0100

    Initial app commit

---
+++ app/src/main/java/beg/hr/todos/MainActivity.kt
@@ -0,0 +1,11 @@
+package beg.hr.todos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
